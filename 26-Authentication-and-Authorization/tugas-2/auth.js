b0c2ada63ee32b08ba3e40e2c38e7c6386c12ae8
Hafezd El Daffa <49185736+hafezdeldaffa@users.noreply.github.com>
2021-12-15 18:25:24 +0700

    Update auth.js

--- 26-Authentication-and-Authorization/tugas-2/auth.js
+++ 26-Authentication-and-Authorization/tugas-2/auth.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 4000;
+const port = 3000;
 
 const users = [
   {
@@ -23,32 +23,13 @@ const users = [
 
 const accessTokenSecret = 'youraccesstokensecret';
 
-const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-
-    jwt.verify(token, accessTokenSecret, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-
-      req.user = user;
-      next();
-    });
-  } else {
-    res.sendStatus(401);
-  }
-};
-
 app.post('/login', (req, res, next) => {
   const { username, password } = req.body;
 
   const user = users.filter((el) => el.username === username);
   const passwordUsers = users.filter((el) => el.password === password);
 
-  if (user && passwordUsers && username === 'terra') {
+  if (user && passwordUsers) {
     const token = jwt.sign(
       {
         username: users.username,
@@ -62,61 +43,6 @@ app.post('/login', (req, res, next) => {
   }
 });
 
-/* Books Service */
-
-const books = [
-  {
-    author: 'Robert Martin',
-    country: 'USA',
-    language: 'English',
-    pages: 209,
-    title: 'Clean Code',
-    year: 2008,
-  },
-  {
-    author: 'Dave Thomas & Andy Hunt',
-    country: 'USA',
-    language: 'English',
-    pages: 784,
-    title: 'The Pragmatic Programmer',
-    year: 1999,
-  },
-  {
-    author: 'Kathy Sierra, Bert Bates',
-    country: 'USA',
-    language: 'English',
-    pages: 928,
-    title: 'Head First Java',
-    year: 2003,
-  },
-];
-
-app.get('/books', authenticateJWT, (req, res, next) => {
-  res.json(books);
-});
-
-app.post('/books', authenticateJWT, (req, res, next) => {
-  const { author, country, language, pages, title, year } = req.body;
-
-  const { role } = req.user;
-
-  if (role != "admin") {
-    return res.sendStatus(403);
-  }
-  
-  const newBook = {
-    author: author,
-    country: country,
-    language: language,
-    pages: pages,
-    title: title,
-    year: year,
-  };
-
-  books.push(newBook);
-  res.status(201).send('Book added successfully');
-});
-
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:4000`);
+  console.log(`Server listening on http://localhost:3000`);
 });
